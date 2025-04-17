ce3e213eb59a46e0e75bfa5a30a5326fcbea28c7
Mayank bisht <mayankbisht1107@gmail.com>
2025-04-18 00:23:43 +0530

    Updated Team  pages

--- my-app/src/Pages/Alumini.jsx
+++ my-app/src/Pages/Alumini.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Batch2024 from '../components/Batch2024';
-import Batch2025 from '../components/Batch2025'
+import Batch2025 from '../components/Batch2025';
 
 function Alumini() {
   const [selected, setSelected] = useState('Batch2024');
@@ -10,29 +10,29 @@ function Alumini() {
       <h1 className="Alumini-heading">Alumini</h1>
 
       <div className="Alumini-box">
-        <div className="Alumini-box">
-          {selected === 'Batch2024' ? <Batch2024 /> : <Batch2025 />}
-        </div>
+        {selected === 'Batch2024' ? <Batch2024 /> : <Batch2025 />}
       </div>
 
       <div className="Alumini-btn">
-  <div className="full-width-line"></div>
+        <div className="full-width-line"></div>
 
-  <div className="batch-btn-group">
-    <button className="btn-2024" onClick={() => setSelected('Batch2024')}></button>
-    <p className='p-2024'>2023-2024</p>
-  </div>
+        <div className="batch-btn-group">
+          <button
+            className={`btn-2024 ${selected === 'Batch2024' ? 'active' : ''}`}
+            onClick={() => setSelected("Batch2024")}
+          />
+          <p className="p-2024">2023-2024</p>
+        </div>
 
-  <div className="batch-btn-group">
-    <button className="btn-2025" onClick={() => setSelected('Batch2025')}></button>
-    <p className='p-2025'>2024-2025</p>
-  </div>
-</div>
-
-
-
-
-  </div>
+        <div className="batch-btn-group">
+          <button
+            className={`btn-2025 ${selected === 'Batch2025' ? 'active' : ''}`}
+            onClick={() => setSelected("Batch2025")}
+          />
+          <p className="p-2025">2024-2025</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- my-app/src/Pages/Team.jsx
+++ my-app/src/Pages/Team.jsx
@@ -2,7 +2,68 @@ import React from 'react'
 
 function Team() {
   return (
-    <div>Team</div>
+    <div>
+      <div className='head-team'>
+        <h1 className='team-head'>Team</h1>
+      </div>
+      <div className="team-card-1">
+      <div className="container-team">
+        <div className='container-photo'>photo</div>
+        <div className='container-details'>
+          <div className="basic-detail-team">Xyz 
+            Lead
+          </div>
+          <div className="links-team">
+            <img width="52px" height="53px" src="src\assets\git.png" alt="git" />
+          <img width="60px" height="60px" src="src\assets\in.webp" alt="Linkdin" /></div>
+
+        </div>
+      </div>
+      
+      </div>
+      <div className="team-card-2">
+      <div className="container-team">
+        <div className='container-photo'>photo</div>
+        <div className='container-details'>
+          <div className="basic-detail-team">Xyz 
+            Lead
+          </div>
+          <div className="links-team">
+            <img width="52px" height="53px" src="src\assets\git.png" alt="git" />
+          <img width="60px" height="60px" src="src\assets\in.webp" alt="Linkdin" /></div>
+
+        </div>
+      </div>
+      </div>
+      <div className="team-card-3">
+      <div className="container-team">
+        <div className='container-photo'>photo</div>
+        <div className='container-details'>
+          <div className="basic-detail-team">Xyz 
+            Lead
+          </div>
+          <div className="links-team">
+            <img width="52px" height="53px" src="src\assets\git.png" alt="git" />
+          <img width="60px" height="60px" src="src\assets\in.webp" alt="Linkdin" /></div>
+
+        </div>
+      </div>
+      </div>
+      <div className="team-card-4">
+      <div className="container-team">
+        <div className='container-photo'>photo</div>
+        <div className='container-details'>
+          <div className="basic-detail-team">Xyz 
+            Lead
+          </div>
+          <div className="links-team">
+            <img width="52px" height="53px" src="src\assets\git.png" alt="git" />
+          <img width="60px" height="60px" src="src\assets\in.webp" alt="Linkdin" /></div>
+
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
