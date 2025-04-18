c9e06a0a77b78d0ff974a037884a83e09c6337ef
Mayank bisht <mayankbisht1107@gmail.com>
2025-04-19 00:08:29 +0530

    Updated contact  pages but not completely done

--- my-app/src/Pages/Contact.jsx
+++ my-app/src/Pages/Contact.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <div className="contact-head">
+        <div className="head-contact">
+          Contact us
+        </div>
+      </div>
+
+      <div className="contact-main">
+        <div className="contact-details">
+          <div className="contact-1">
+            <img src="/src/assets/download.jpeg" alt="Profile" />
+          </div>
+          <div className="contact-2">
+            <p>Where Ideas Spark,</p> 
+            <p>and Tech Ignites!⚡</p>
+          </div>
+          <div className="contact-3">
+            <img src="/src/assets/linkdin.png" alt="LinkedIn" />
+            <img src="/src/assets/email.png" alt="Email" />
+            <img src="/src/assets/instagram.png" alt="Instagram" />
+          </div>
+        </div>
+        <code>
+        <div className="contact-bottom">
+        <p>Made with 💙  by IoSC-EDC
+        One Club, One Family</p>
+        </div>
+        </code>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
